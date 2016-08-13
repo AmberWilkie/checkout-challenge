f228f1d1359affab3523746e3a14735cc277b48f
Amber <amber@amberwilkie.com>
2016-08-13 12:17:09 +0200

    Update rubocop file so it stops complaining

--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -1,3 +1,15 @@
 require './lib/checkout.rb'
 
-describe 'Checkout'
+describe 'Checkout' do
+
+  it 'has a list of items' do
+    expect(subject.blanket).not_to be nil
+  end
+
+  before { subject.scan(blanket)}
+  it 'allows user to add items to cart' do
+    expect(subject.cart).not_to be nil
+  end
+
+
+end
