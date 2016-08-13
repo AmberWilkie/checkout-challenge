38fea044937cc20c2facb31ac9b98bb6a0533f61
Amber <amber@amberwilkie.com>
2016-08-13 16:06:32 +0200

    Discount hot dogs to 8.25 if >2 are in cart

--- lib/checkout.rb
+++ lib/checkout.rb
@@ -2,29 +2,45 @@ require 'pry'
 
 class Checkout
 
-  attr_accessor :total, :cart, :blanket, :lounger, :hotdog, :pre_price
+  attr_accessor :total, :cart, :blanket, :lounger, :hotdog, :pre_price, :hotdog_hash
 
   def initialize
     #some kind of switch between promotions
     @cart = []
     @blanket = {code: 1, desc: 'A soft and fluffy blanket', price: 19.95}
-    @lounger = {code: 2, desc: 'A sturdy and attractive beach chair', price: 45}
+    @lounger = {code: 2, desc: 'A sturdy and attractive beach chair', price: 45.0}
     @hotdog = {code: 3, desc: 'An overpriced meat product', price: 9.25}
   end
 
   def scan(item)
     @cart << item
-    #the search of the hash and the values paired with "price" added together.
-    # hash_of_all_prices = cart.select {|item| item.include? }
+
+    #give us the count of each item in the cart
+    count = Hash.new(0)
+    cart.each do |item| count[item] += 1
+    end
+
+    #give us just the hot dogs from the count
+    if count[hotdog] >= 2
+       rewrite_cart
+    end
+
     pre_price = 0
     cart.each do |item|
       pre_price += item[:price]
     end
     # binding.pry
     pre_price > 60.0 ? @total = pre_price*0.9 : @total = pre_price
-
   end
 
+  private
+  def rewrite_cart
+    cart.each do |item|
+      if item[:code] == 3
+        item[:price] = 8.25
+      end
+    end
+  end
 
 
 
--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -11,17 +11,18 @@ describe Checkout do
 
   end
 
-  before do
-    subject.scan(subject.blanket)
-  end
-  it 'allows user to add items to cart' do
-    expect(subject.cart).not_to eq []
-  end
+  describe 'Basics' do
 
-  it 'totals items in the cart' do
-    expect(subject.total).to eq 19.95
-  end
+    before { subject.scan(subject.blanket) }
 
+    it 'allows user to add items to cart' do
+      expect(subject.cart).not_to eq []
+    end
+
+    it 'totals items in the cart' do
+      expect(subject.total).to eq 19.95
+    end
+  end
   describe 'Subject has checked out more than $60 of items' do
 
     before do
@@ -36,19 +37,26 @@ describe Checkout do
     end
 
     it 'gives a 10% discount when total goes over $60' do
-      expect(subject.total).to eq 139.15*0.9
+      expect(subject.total).to eq 119.2*0.9
     end
   end
 
   describe 'User has checked out two hotdogs' do
     before do
-      subject.create_items
-      subject.scan(hotdog)
-      subject.scan(hotdog)
+      subject.scan(subject.hotdog)
+      subject.scan(subject.hotdog)
     end
+
+    # it 'counts the number of hot dogs in cart' do
+    #   expect(subject.count).not_to be nil
+    # end
 
     it 'reduces cost of hot dogs to 8.25' do
       expect(subject.total).to be 16.50
     end
+
+    #Need to deal with combining promos
+    #Need to deal with having a product hash instead of just set variables
+    #Might want to let a user remove a product from their cart (affecting hotdog discounts)
   end
 end
